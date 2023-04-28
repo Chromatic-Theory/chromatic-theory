e42d012eab884859f796ff6d6a769e434df163a8
Alvin Quach <alvinwquach@gmail.com>
2023-04-27 17:13:27 -0700

    Add sanity and create schemas

---
+++ chromatic-theory/schemas/about.ts
@@ -0,0 +1,55 @@
+const about = {
+  name: 'about',
+  title: 'About',
+  type: 'document',
+  fields: [
+    {
+      name: 'selfie',
+      title: 'Selfie',
+      type: 'image',
+      description: 'Please upload an image of yourself.',
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'heroImage',
+      title: 'Hero (Image)',
+      type: 'image',
+      description: 'Please upload an image of your salon logo.',
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'story',
+      title: 'Story',
+      type: 'array',
+      description: 'Please provide your story here.',
+      of: [{type: 'block'}],
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'instagram',
+      title: 'Instagram',
+      type: 'url',
+      description: 'Please provide the link to your Instagram here.',
+    },
+    {
+      name: 'yelp',
+      title: 'Yelp',
+      type: 'url',
+      description: 'Please provide the link to your Instagram here.',
+    },
+    {
+      name: 'booking',
+      title: 'Booking',
+      type: 'url',
+      description: 'Please provide the link to book appointments here.',
+    },
+  ],
+}
+
+export default about
